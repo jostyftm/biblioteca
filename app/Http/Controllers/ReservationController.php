3880318fecee869dfacc9259b9da44467b2e8614
Joe Valencia <stiwart@gmail.com>
2021-07-12 02:22:10 -0500

    List reservations

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -14,7 +14,10 @@ class ReservationController extends Controller
      */
     public function index()
     {
-        return view('pages.admin.reservation.index');
+        $reservations = Reservation::with('student.user')
+        ->paginate(5);
+
+        return view('pages.admin.reservation.index', compact('reservations'));
     }
 
     /**
--- resources/views/pages/admin/reservation/index.blade.php
+++ resources/views/pages/admin/reservation/index.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.dashboard')
 
+@section('header_section')
+    <div class="d-flex justify-content-between align-items-center">
+        <h1>Prestamos</h1>
+        <a href="{{route('reservations.create')}}" class="btn btn-sm btn-primary">Nuevo prestamo</a>
+    </div>
+@endsection
 @section('content_main')
+    <div class="bg-white p-4 rounded shadow-sm">
+        <div class="table-responsible">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Codigo</th>
+                        <th>Estudiante</th>
+                        <th>Estado</th>
+                        <th>Fecha de prestamo</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($reservations as $reservation)
+                    <tr>
+                        <td>{{ $reservation->id}}</td>
+                        <td>{{ $reservation->student->user->name }}</td>
+                        <td>{{ $reservation->reservation_state }}</td>
+                        <td>{{ $reservation->reservated_at }}</td>
+                        <td>
+                            <button class="btn btn-sm btn-primary">
+                                Editar
+                            </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
+            {{ $reservations->links() }}
+        </div>
+    </div>
 @endsection
