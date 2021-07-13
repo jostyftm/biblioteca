9f4ab301154f1d44cd9dd1f593aad2fcd3809f21
Joe Valencia <stiwart@gmail.com>
2021-07-13 03:57:17 -0500

    Request to create Reservation

---
+++ app/Http/Requests/CreateReservationRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\ExistingBookRule;
+use App\Rules\ExistingStudentRule;
+use App\Rules\StockBookRule;
+use App\Rules\StudentHasReservationPendingRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'student_code'  =>  [
+                'required', 
+                new ExistingStudentRule(),
+                new StudentHasReservationPendingRule(),
+            ],
+            'book_code'  =>  [
+                'required',
+                new ExistingBookRule(),
+                new StockBookRule(),
+            ]
+        ];
+    }
+}
